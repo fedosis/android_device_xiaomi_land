9f2ece51ddef0667f04ff12825887bae62a5782d
piiiggg <hiimpig6@gmail.com>
2017-06-07 17:01:09 +0700

    Try to re-make BoardConfig.mk again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,15 +17,8 @@
 DEVICE_PATH := device/xiaomi/land
 BOARD_PATH := device/xiaomi/land/board
 
-# DEODEX BUILD
-#DISABLE_DEXPREOPT=true
-
-# Define platform before including any common things
-include $(DEVICE_PATH)/PlatformConfig.mk
-
-# Inherit common ARM64 board fragments
-include $(BOARD_PATH)/common/arm64/architecture.mk
-include $(BOARD_PATH)/common/arm64/binder.mk
+# Land Device
+include $(DEVICE_PATH)/land.mk
 
 # Inherit common board fragments
 include $(BOARD_PATH)/common/bluetooth.mk
@@ -57,9 +50,6 @@ include $(BOARD_PATH)/qcom/time.mk
 
 # Inherit Cyanogen board fragments
 include $(BOARD_PATH)/cyanogen/hardware.mk
-
-# Inherit device-specific board fragments
-include $(DEVICE_PATH)/board/*.mk
 
 # Inherit the proprietary files
 -include vendor/xiaomi/land/BoardConfigVendor.mk
--- lineage.mk
+++ lineage.mk
@@ -16,7 +16,7 @@
 #
 
 # Define platform before including any common things
-$(call inherit-product, $(LOCAL_PATH)/PlatformConfig.mk)
+TARGET_BOARD_PLATFORM := msm8937
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
